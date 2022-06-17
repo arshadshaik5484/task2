feb525cc49277560290429ff0bdef38cc44c8f9a
arshadshaik5484 <103424104+arshadshaik5484@users.noreply.github.com>
2022-06-17 17:00:10 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  $host = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbName = "user1";
+  
+  $mysqli = mysqli_connect($host, $dbUsername, $dbPassword, $dbName);
+?>
---
+++ user.php
@@ -0,0 +1,2 @@
+<h1>user page</h1>
+<p>this is a Arshad user page</p>
